2a1b6f3c5b19a6b9a259b4f6da57ecb17240d4f8
CHAN JUN PARK <wns0415@gmail.com>
2021-12-17 01:17:47 +0900

    update chapter 1.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,7 +1,9 @@
 package com.company;
 
 import java.io.Console;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -11,8 +13,34 @@ public class Main {
 //        practiceInput();
 //        practiceConsole(); -> 책에 있는대로 작성했는데 안 됨;;
 //        practiceArrayList();
-        practiceEnhancedLoop();
+//        practiceEnhancedLoop();
+        practiceCopyArray();
 
+    }
+
+    public static void practiceCopyArray(){
+        int[] primes = {1, 2, 3, 4, 5, 6};
+        int[] numbers = primes; // 같은 배열을 참조하는 두 배열
+
+        int[] copiedPrimes = Arrays.copyOf(primes, primes.length); // 새로운 배열을 원하는 길이로 생성하고 요소를 복사
+
+        ArrayList<String> friends = new ArrayList<>();
+        friends.add("John");
+        friends.add("Kane");
+
+        ArrayList<String> people = friends;
+        System.out.println(people.get(0));
+
+        people.set(0, "Mary"); // friends.get(0)도 Mary가 된다.
+        System.out.println(people.get(0));
+        System.out.println(friends.get(0));
+
+        ArrayList<String> copiedFriends = new ArrayList<>(friends); // ArrayList에도 똑같이 복사 적용 가능.
+        System.out.println(copiedFriends.get(0));
+
+        copiedFriends.set(0, "Messi");
+        System.out.println(friends.get(0));
+        System.out.println(copiedFriends.get(0));
     }
 
     public static void practiceEnhancedLoop(){
